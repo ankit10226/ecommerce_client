07c8e11c8d1e71b9666c60839f90f883ae016a36
ankit10226 <152682906+ankit10226@users.noreply.github.com>
2025-03-22 11:02:36 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,8 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+ import './App.css'
 import AppRouter from './routes/AppRouter'
 import Modal from './components/UI/Modal/Modal'
 
-function App() {
-  const [count, setCount] = useState(0)
+function App() { 
 
   return (
     <>
