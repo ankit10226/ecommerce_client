dc2e3d6fbd0b99c0df6bdc6ba7ac71d14131c19a
ankit10226 <152682906+ankit10226@users.noreply.github.com>
2025-03-22 11:04:38 +0530

    Add files via upload

---
+++ App.jsx
@@ -0,0 +1,15 @@
+import './App.css'
+import AppRouter from './routes/AppRouter'
+import Modal from './components/UI/Modal/Modal'
+
+function App() { 
+
+  return (
+    <>
+      <Modal />
+      <AppRouter />
+    </>
+  )
+}
+
+export default App
