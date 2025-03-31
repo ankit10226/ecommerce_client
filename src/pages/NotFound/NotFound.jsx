bf7d2e2a298288986c75eab7959576339959c0ca
ankit10226 <152682906+ankit10226@users.noreply.github.com>
2025-03-31 21:58:05 +0530

    Update NotFound.jsx

--- src/pages/NotFound/NotFound.jsx
+++ src/pages/NotFound/NotFound.jsx
@@ -1,12 +1,14 @@
 import React, { useEffect } from 'react'
 import { useDispatch } from 'react-redux'
 import { logoutUser } from '../../redux/slices/AuthSlice';
+import Button from '../../components/UI/Button/Button';
+import { Link } from 'react-router-dom';
 
 const NotFound = () => {
-  const dispatch = useDispatch();
-  useEffect(() => { 
-    dispatch(logoutUser());
-  }, [dispatch])
+  // const dispatch = useDispatch();
+  // useEffect(() => { 
+  //   dispatch(logoutUser());
+  // }, [dispatch])
   
   return (
     <div className='w-screen h-screen flex flex-col justify-center items-center bg-cyan-100'>
@@ -14,6 +16,9 @@ const NotFound = () => {
       <p className="text-xl mb-8">
         The page you are looking for does not exist.
       </p>
+      <Link to={'/dashboard'}>
+        <Button className="bg-red-400 text-white">Go To Dashboard</Button>
+      </Link>
     </div>
   )
 }
