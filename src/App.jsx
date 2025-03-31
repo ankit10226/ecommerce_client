9fd08eec42b0dc2463b59894ed86d36490314113
ankit10226 <152682906+ankit10226@users.noreply.github.com>
2025-03-31 21:57:15 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,16 @@
 import './App.css'
 import AppRouter from './routes/AppRouter'
 import Modal from './components/UI/Modal/Modal'
+import { checkUserSession } from './redux/slices/AuthSlice' 
+import { useEffect } from 'react'
+import { useDispatch } from 'react-redux'
 
 function App() { 
-
+  const dispatch = useDispatch();
+  useEffect(() => {
+    dispatch(checkUserSession());
+  }, [dispatch])
+  
   return (
     <>
       <Modal />
