34e90bda94ef8b7e06baddbd18775ecaaf190444
ankit10226 <152682906+ankit10226@users.noreply.github.com>
2025-04-02 11:14:07 +0530

    Update NotFound.jsx

--- src/pages/NotFound/NotFound.jsx
+++ src/pages/NotFound/NotFound.jsx
@@ -1,10 +1,11 @@
 import React, { useEffect } from 'react'
-import { useDispatch } from 'react-redux'
+import { useDispatch, useSelector } from 'react-redux'
 import { logoutUser } from '../../redux/slices/AuthSlice';
 import Button from '../../components/UI/Button/Button';
 import { Link } from 'react-router-dom';
 
 const NotFound = () => {
+  const { user } = useSelector((state)=>state.auth);
   // const dispatch = useDispatch();
   // useEffect(() => { 
   //   dispatch(logoutUser());
@@ -16,7 +17,7 @@ const NotFound = () => {
       <p className="text-xl mb-8">
         The page you are looking for does not exist.
       </p>
-      <Link to={'/dashboard'}>
+      <Link to={user?.role === 'user' ? '/user/dashboard' : '/admin/dashboard'}>
         <Button className="bg-red-400 text-white">Go To Dashboard</Button>
       </Link>
     </div>
