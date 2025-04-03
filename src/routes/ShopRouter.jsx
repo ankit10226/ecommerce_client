ec2254e84f45d96141efef8d9e9c49fccd9d2d99
ankit10226 <152682906+ankit10226@users.noreply.github.com>
2025-04-03 12:53:54 +0530

    Update ShopRouter.jsx

--- src/routes/ShopRouter.jsx
+++ src/routes/ShopRouter.jsx
@@ -1,1 +1,38 @@
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import { lazy, Suspense } from 'react';
 
+import Loading from '../components/UI/Loading/Loading';
+const ShopDashboard = lazy(() =>
+  import('../pages/Shop/Dashboard/ShopDashboard')
+);
+const NotFound = lazy(() => import('../pages/NotFound/NotFound'));
+const ProtectedRoutes = lazy(() => import('./ProtectedRoutes'));
+
+const ShopRouter = () => {
+  const router = createBrowserRouter([
+    {
+      path: '/shop/dashboard',
+      element: (
+        <ProtectedRoutes>
+          <ShopDashboard />
+        </ProtectedRoutes>
+      ),
+    },
+    {
+      path: '*',
+      element: (
+        <ProtectedRoutes>
+          <NotFound />
+        </ProtectedRoutes>
+      ),
+    },
+  ]);
+
+  return (
+    <Suspense fallback={<Loading />}>
+      <RouterProvider router={router} />
+    </Suspense>
+  );
+};
+
+export default ShopRouter;
