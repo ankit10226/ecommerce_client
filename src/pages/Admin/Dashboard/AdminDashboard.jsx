f0c627b1c6e58aba3db9cdb7b9a221f3e91dfa46
ankit10226 <152682906+ankit10226@users.noreply.github.com>
2025-04-03 10:09:16 +0530

    Update AdminDashboard.jsx

--- src/pages/Admin/Dashboard/AdminDashboard.jsx
+++ src/pages/Admin/Dashboard/AdminDashboard.jsx
@@ -1,9 +1,30 @@
-import React from 'react'
+import React from 'react';
+import Button from '../../../components/UI/Button/Button';
+import { useDispatch } from 'react-redux';
+import { destroyUserSession } from '../../../redux/slices/AuthSlice';
+import { useNavigate } from 'react-router-dom';
+import { showModal } from '../../../redux/slices/ModalSlice';
 
 const AdminDashboard = () => {
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+  const handleLogout = () => {
+    dispatch(destroyUserSession());
+    navigate('/', { replace: true });
+    dispatch(showModal({ type: 'success', message: 'Logout Successfully.' }));
+  };
   return (
-    <div>AdminDashboard</div>
-  )
-}
+    <>
+      <div>AdminDashboard</div>
+      <Button
+        type="button"
+        className="bg-red-400 text-white"
+        onClick={handleLogout}
+      >
+        Logout
+      </Button>
+    </>
+  );
+};
 
-export default AdminDashboard
+export default AdminDashboard;
