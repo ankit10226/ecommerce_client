5f3bcbf8479b23196f2d97b1e7cd17073af17798
ankit10226 <152682906+ankit10226@users.noreply.github.com>
2025-04-04 17:53:45 +0530

    Update Button.jsx

--- src/components/UI/Button/Button.jsx
+++ src/components/UI/Button/Button.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Button = (props) => {
   return (
-    <button type={props.type || "button"} className={`font-semibold py-1 px-3 rounded-lg cursor-pointer ${props.className}`} onClick={props.onClick}>
+    <button type={props.type || "button"} className={`font-semibold py-1 px-3 rounded-sm cursor-pointer ${props.className}`} onClick={props.onClick}>
       {props.children}
     </button>
   )
