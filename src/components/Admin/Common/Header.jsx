a54294b41684104012a88b044fdece6bf772a098
ankit10226 <152682906+ankit10226@users.noreply.github.com>
2025-04-06 00:08:39 +0530

    Update Header.jsx

--- src/components/Admin/Common/Header.jsx
+++ src/components/Admin/Common/Header.jsx
@@ -1,9 +1,10 @@
 import React from 'react'
 import Button from '../../UI/Button/Button'
-import { AlignJustify } from 'lucide-react'
+import { AlignJustify, LogOut } from 'lucide-react'
 import { useDispatch } from 'react-redux'
 import { useNavigate } from 'react-router-dom'
 import { showModal } from '../../../redux/slices/ModalSlice'
+import { destroyUserSession } from '../../../redux/slices/AuthSlice'
 
 const Header = () => {
   const dispatch = useDispatch();
@@ -13,9 +14,12 @@ const Header = () => {
     navigate('/',{ replace: true });
     dispatch(showModal({ type: 'success', message: 'Logout Successfully.' }));
   }
+  const showSidebar = () =>{
+    
+  }
   return (
-    <header className='h-16 w-full shadow-lg flex items-center justify-between px-10'> 
-      <Button type="button" className="bg-blue-950 text-white">
+    <header className='h-16 w-full shadow-lg flex items-center justify-between lg:justify-end px-4'> 
+      <Button type="button" className="bg-red-400 text-white lg:hidden md:block" onClick={showSidebar}>
         <AlignJustify />
       </Button>
       <Button
@@ -23,7 +27,7 @@ const Header = () => {
         className="bg-red-400 text-white"
         onClick={handleLogout}
       >
-        Logout
+       <LogOut className='inline'/>Logout
       </Button>
     </header>
   )
