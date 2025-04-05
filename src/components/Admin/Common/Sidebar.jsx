ea195598230692124f6d4c15f24518465c85a079
ankit10226 <152682906+ankit10226@users.noreply.github.com>
2025-04-06 00:10:59 +0530

    Update Sidebar.jsx

--- src/components/Admin/Common/Sidebar.jsx
+++ src/components/Admin/Common/Sidebar.jsx
@@ -1,13 +1,18 @@
 import React from 'react'
+import Logo from './Logo'
+import sidebarList from '../../../utils/sidebarList/sidebarList'
+import { Link } from 'react-router-dom'
 
 const Sidebar = () => {
   return (
-    <div className='h-svh w-1/5 shadow-xl'>
-      <div className='h-16 shadow-lg flex justify-center items-center'>LOGO</div>
-      <ul>
-        <li>Dashboard</li>
-        <li>Product</li>
-        <li>Order</li>
+    <div className='hidden h-svh w-1/5 shadow-xl bg-red-400 lg:block md:block sm:block'>
+      <Logo /> 
+      <ul className="text-white py-4 px-6">
+        {
+          sidebarList.map((list)=>(
+            <Link to={list.navigate} key={list.id}><li className='w-fit flex items-center justify-start py-2 cursor-pointer transition duration-200 ease-in-out hover:scale-110'>{list.logo}<span className='pl-2 text-md font-semibold'>{list.name}</span></li></Link>
+          ))
+        } 
       </ul>
     </div>
   )
