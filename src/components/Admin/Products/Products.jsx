ec13dfa984a52fe2fabe1b1cc03b7796e825ba43
Ankit Singh <ankit10226.as@gmail.com>
2025-06-01 19:21:20 +0530

    icon

--- src/components/Admin/Products/Products.jsx
+++ src/components/Admin/Products/Products.jsx
@@ -1,29 +1,28 @@
-import React from 'react';
-import Button from '../../UI/Button/Button';
-import { useDispatch, useSelector } from 'react-redux';
-import { toggleProductModal } from '../../../redux/slices/ProductSlice';
-import AddProductModal from './AddProductModal';
-import ProductDetails from './ProductDetails';
+import React from "react";
+import Button from "../../UI/Button/Button";
+import { useDispatch, useSelector } from "react-redux";
+import { toggleProductModal } from "../../../redux/slices/ProductSlice";
+import AddProductModal from "./AddProductModal";
+import ProductDetails from "./ProductDetails";
 const Products = () => {
   const dispatch = useDispatch();
   return (
-    <div className="w-full"> 
+    <>
       <AddProductModal />
-      <div className="h-fit">
-        <div className="flex justify-between items-center p-2">
-          <h1 className="font-bold text-2xl text-teal-900 underline">
-            Products
-          </h1>
-          <Button
-            className="bg-teal-500 text-white"
-            onClick={() => dispatch(toggleProductModal({type:'addModal'}))}
-          >
-            Add New Product
-          </Button>
-        </div>
+      <div className="flex justify-between items-center p-2">
+        <h1 className="font-bold text-2xl text-teal-900 underline">Products</h1>
+        <Button
+          className="bg-teal-500 text-white"
+          onClick={() => dispatch(toggleProductModal({ type: "addModal" }))}
+        >
+          Add New Product
+        </Button>
       </div>
-      <ProductDetails />
+      <div className="w-full h-[calc(100vh-115px)] overflow-y-hidden">
+        <div className="h-fit"></div>
+        <ProductDetails />
       </div>
+    </>
   );
 };
 
